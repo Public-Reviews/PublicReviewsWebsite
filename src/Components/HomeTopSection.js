f46d0ded6d3a6a89f50209ab216beb6410c9e703
Samuel Khongthaw <b20cs004@nitm.ac.in>
2021-10-23 18:28:59 +0530

    Finalising NavBar and HomeTopSection

--- src/Components/HomeTopSection.js
+++ src/Components/HomeTopSection.js
@@ -5,17 +5,29 @@ export function HomeTopSection() {
     <div id="container" className="p-12 bg-gray-200">
       <div className="flex flex-row justify-between">
         <div className="flex flex-col">
-          <span className="text-5xl font-black flex-wrap  cursor-pointer hover:text-indigo-500 transition duration-300">
+          <span
+            className="text-6xl font-black flex-wrap  cursor-pointer hover:text-indigo-500 transition duration-300"
+            unselectable="on"
+          >
             Public Reviews Website
           </span>
-          <span className="font-mono mt-6 flex-wrap">
+          <span className="font-mono mt-6 flex-wrap" unselectable="on">
             Look up public reviews and ratings of businesses around you.
           </span>
-          <span className="font-mono mt-6 flex-wrap">
+          <span className="font-mono mt-6 flex-wrap" unselectable="on">
             One stop site to view departmental stores, <br />
             cafes, restaurants, hospitals,
             <br /> repairshops the list goes on...
           </span>
+          <div className="text-3xl mt-10 font-blacky">
+            <span
+              className="border-2 rounded-xl border-gray-300 bg-gray-300 p-2 hover:text-white hover:bg-indigo-500 hover:border-indigo-600 transition duration-300 cursor-pointer"
+              unselectable="on"
+            >
+              {" "}
+              Write a review
+            </span>
+          </div>
         </div>
         <div
           id="images"
